61e20ac0339cd50580c8a175d884570ac1c2f9be
Szyln <szyen.lin@gmail.com>
2022-04-20 13:29:36 +0800

    Adding dist

--- src/component/LinkToPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-const LinkToPage = ({to, children}) => {
-  return (
-    <Link to={`/${to}`} className='d-flex text-decoration-none mb-4 mb-md-0'>{children}<span className='material-icons ms-2'>arrow_right_alt</span></Link>
-  )
-}
-
-export default LinkToPage
